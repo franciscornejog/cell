5ff049cd987c2b675d18c657b941eca68975ba60
Francisco Cornejo-Garcia <franciscornejog@gmail.com>
2023-02-25 18:23:36 -0500

    feat: show player

---
+++ cell-game-design-doc.md
@@ -0,0 +1,28 @@
+Project Name: Cell
+A semi-collaborative 2-player shooter to destroy increasingly difficult cells 
+with randomly generated levels and real-time movement.
+
+2 cells must seek out and destroy all enemy cells before they die.
+
+Basic Game Loops
+1. Enter organism
+2. Find and kill enemy cells 
+3. Power-ups?
+4. Move to next level
+
+Minimum Viable Product 
+1. Create basic map
+2. Create and move player
+3. Player can shoot particles
+3. Spawn enemy cells
+4. Kill enemy cells with particles
+5. Show "Game Over" when player dies
+6. Player win by killing all enemy cells
+
+Stretch Goals 
+1. Add second player
+2. Player can drop a virus (bomb)
+3. Add barriers (membrane vs cell wall)
+4. Add score system
+5. Create more levels
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct SnakeHead;
+
+const PLAYER_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_camera)
+        .add_startup_system(spawn_player)
+        .add_system(hello)
+        .run();
+}
+
+fn add_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: PLAYER_COLOR,
+            ..default()
+        },
+        transform: Transform {
+            scale: Vec3::new(10.0, 10.0, 10.0),
+            ..default()
+        },
+        ..default()
+    }).insert(SnakeHead);
+}
+
+fn hello() {
+    println!("hello");
 }
