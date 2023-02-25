3669c4daa5996e7fd79e4f873f262130c98ac78e
Francisco Cornejo-Garcia <franciscornejog@gmail.com>
2023-02-25 18:52:09 -0500

    feat: move player

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct SnakeHead;
+struct Player;
 
 const PLAYER_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_camera)
+        .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
-        .add_system(hello)
+        .add_system(move_player)
         .run();
 }
 
-fn add_camera(mut commands: Commands) {
+fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
@@ -24,14 +24,24 @@ fn spawn_player(mut commands: Commands) {
             color: PLAYER_COLOR,
             ..default()
         },
-        transform: Transform {
-            scale: Vec3::new(10.0, 10.0, 10.0),
-            ..default()
-        },
+        transform: Transform::from_scale(Vec3::new(10.0, 10.0, 10.0)),
         ..default()
-    }).insert(SnakeHead);
+    }).insert(Player);
 }
 
-fn hello() {
-    println!("hello");
+fn move_player(key: Res<Input<KeyCode>>, mut positions: Query<&mut Transform, With<Player>>) {
+    for mut transform in positions.iter_mut() {
+        if key.pressed(KeyCode::H) {
+            transform.translation.x -= 2.0;
+        }
+        if key.pressed(KeyCode::L) {
+            transform.translation.x += 2.0;
+        }
+        if key.pressed(KeyCode::J) {
+            transform.translation.y -= 2.0;
+        }
+        if key.pressed(KeyCode::K) {
+            transform.translation.y += 2.0;
+        }
+    }
 }
